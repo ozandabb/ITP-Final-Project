385858d6ca6a19030e36550e9cad51e92abfaadf
Jayamadu Thisun <jayamaduthisun@gmail.com>
2019-09-23 22:16:42 +0530

    Test

---
+++ StudentApp/Controllers/EventController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentApp.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hello() {
+            return View();
+        }
+    }
+}
